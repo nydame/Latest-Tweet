7bf56cbc35b78575c73ab2fc69fafa56ea665c2e
Felicia Betancourt <fmbetancourt@gmail.com>
2012-10-25 15:02:58 -0700

    changes to README.md and to latest-tweet.js, plus minification of the latter

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
 ### Welcome!
 
-Latest Tweet is (thus far) a bare bones jQuery plugin that displays the latest tweet of a given entity on Twitter on a website.  The username and number of tweets to search for a valid tweet can be supplied as parameters.  Valid tweets are neither retweets nor replies (i.e., tweets starting with "@"). Eventually a WordPress plugin will be developed through increasingly feature-rich branches. 
+Latest Tweet is (thus far) a bare bones jQuery plugin that displays the most recent tweet of a given entity on Twitter on a website.  The username and the number of tweets to search for a valid tweet can be supplied as parameters.  Valid tweets are neither retweets nor replies (i.e., tweets starting with "@").  If no valid tweets are found, nothing is displayed.
+
+Eventually a WordPress plugin will be developed. 
 
 To use 
-1. create a div with id="twitter" in your markup to contain the tweet, 
-2. download latest-tweet.js., and edit the values of *username* and (if desired) *num*
-3. upload the resulting code to your website, either as a separate file or added to an existing javascript file
+1. Create a div with id="twitter" in your markup to contain the tweet.
+2. Download latest-tweet.js or latest-tweet-min.js. and open it in a text editor.
+3. Find 
+		latestTweet();
+near the very end of the code and change it to
+		latestTweet("twittername");
+where **twittername** is the desired Twitter account name.  And the quote marks are essential, by the way! If you would like to change the number of tweets that are searched from the default value of 20, add that as a second parameter.  For example,
+		latestTweet("twitterhandle", 10);
+A large value may be desirable for someone who retweets a lot.
+4. Finally upload the resulting code to your website, either as a separate file or added to an existing javascript file.
--- latest-tweet.js
+++ latest-tweet.js
@@ -1,41 +1,47 @@
 //jQuery plugin (wrapped in a self-invoking function) fetches latest tweet
 (function($){
 	$.fn.latestTweet = function(username, num) {
-		if(!username || username === "") {username="nydame";} // default is me :P
+		if(!username || username === "") {username="nydame";} // default is the developer :P
 		if(!num || num<=0) {num = 20;} // default is 20
 		var $orig = this;
 		var tweetString = "",
 		tweetUrl = 'http://api.twitter.com/1/statuses/user_timeline.json?callback=?';
+		function tweetFailHandler() {
+			if(document.getElementById('twitter')) {
+				$('#twitter').hide();
+			}
+			return $orig;
+		}
 		function tweetHandler(data) {
-			if(!data || data.length <= 0) {return $orig;} //FAIL BETTER!!!!!!
+			if(!data || data.length <= 0) {tweetFailHandler();} 
 			var d1 = [],
 			dateString = "",
 			i = 0, //index of latest tweet
 			patt = /^@/;
-			//filter out retweets (null text) and replies (starts w @)
+			//filter out retweets (null text) and replies (start with "@")
 			try {
-				while( data[i].text===null || patt.test(data[i]) ) {
-					i++;
+				while( data[i].text===null || patt.test(data[i].text) ) {
+					i++; 
 					if( i == num ) {break;} 
 				}
 			} catch(e1) {
-				return $orig; //FAIL BETTER!!!!!!
+				tweetFailHandler();
 			}
-			if( i == num ) {return $orig;} //FAIL BETTER!!!!!!!
+			if( i == num ) {tweetFailHandler();}
 			//with value of i set, compose text
 			//1st turn date of latest tweet into an array of its components
 			try {
 				d1 = (data[i].created_at).split(' ');
 				dateString += ' &mdash;&nbsp;'  + d1[0] + ' ' + d1[1] + ' ' + d1[2] + ', ' + d1[5];
 			} catch(e2) {
-				return $orig; //FAIL BETTER!!!!!!!!
+				tweetFailHandler();
 			}
 			tweetString += data[i].text + dateString;
 			//finally, display tweet and return caller
 			if( tweetString !== "" && document.getElementById('twitter') ) {
 				$('#twitter').empty();
-				$('<div id="tweet"><p class="status"><strong>Latest tweet</strong><br /></p></div>').appendTo('#twitter');
-				$('#tweet p.status').append(tweetString);
+				$('<p class="ltw-status"><span>Latest Tweet</span> </p>').appendTo('#twitter');
+				$('#twitter p.ltw-status').append(tweetString);
 			}
 		} //end tweetHandler 
 		//fetch latest tweet
@@ -48,7 +54,7 @@
 	}; //end plugin
 })(jQuery);
 
-//call plugin usig no-conflict wrapper recommended in WP Codex
+//call plugin using no-conflict wrapper recommended in WP Codex
 jQuery(document).ready(function($) {
     // $() will work as an alias for jQuery() inside of this function
 	$(document).latestTweet(); //default username and number to search
